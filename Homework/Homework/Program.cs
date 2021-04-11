8d72fad71aed349656f861202ebefac27a490dad
Sergei Voitko <servojtko84@gmail.com>
2021-04-11 14:19:12 +0300

    Homework solution aws added

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var homework = new HomeWork();
+            var result = homework.InvokePriceCalculatiion();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
