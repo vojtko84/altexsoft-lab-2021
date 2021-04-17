d91cb604e665b6b7a249d8084408b575f9a408ed
Sergei Voitko <servojtko84@gmail.com>
2021-04-17 23:49:58 +0300

    basic types used

--- mydelivery/mydelivery/Models/Order.cs
+++ mydelivery/mydelivery/Models/Order.cs
@@ -5,7 +5,7 @@ namespace Mydelivery.Models
     public class Order : BaseModel
     {
         public int BuyerId { get; set; }
-        public List<Product> Products { get; set; }
+        public IList<Product> Products { get; set; }
         public DeliveryAddress DeliveryAddress { get; set; }
     }
 }
