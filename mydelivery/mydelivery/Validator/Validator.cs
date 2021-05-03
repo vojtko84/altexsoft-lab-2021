1540080c064c232803c79ad1d496702e147bbe99
Sergei Voitko <servojtko84@gmail.com>
2021-05-03 19:07:33 +0300

    Added validator

---
+++ mydelivery/mydelivery/Validator/Validator.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyDelivery.Validator
+{
+    public static class Validator
+    {
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            var pattern = @"^([+]38)?0[(]?\d{2}[)]?\s?\d{3}\s?\d{2}\s?\d{2}$";
+            var regex = new Regex(pattern);
+            return regex.IsMatch(phoneNumber);
+        }
+
+        public static bool IsValidDeliveryAddres(string deliveryAddres)
+        {
+            var pattern = @"^(ул\.|улица\s)[А-я)]+(\.|,)\s?(д\.|дом)\s?\d+(,\s?(кв\.|квартира)\s?\d+)?$";
+            var regex = new Regex(pattern);
+            return regex.IsMatch(deliveryAddres);
+        }
+    }
+}
